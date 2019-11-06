6ea2988cbfd43ddb4cea89cb3cdcdf2b9341ad7a
Ben Spain <ben.spain@target.com>
2019-11-06 15:46:24 -0600

    Base scaffolding for client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+class App extends React.Component {
+  render(): JSX.Element {
+    // const SSR_DATA = document.getElementById('__CR_SERIES_SSR_DATA').innerText;
+    // const payload = JSON.parse(SSR_DATA) as JenkinsJsonPayload;
+
+    return <div className="App">Hello World</div>;
+  }
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  resolve: { extensions: [".js", ".ts", ".tsx"] },
+  module: {
+    rules: [
+      {
+        // All files with '.ts' or '.tsx' extension will be handled by 'ts-loader'
+        test: /\.tsx?$/, 
+        loader: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "public/"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, "public/"), 
+    // port: 3000,
+    // publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
