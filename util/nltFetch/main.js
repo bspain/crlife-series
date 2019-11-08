f0898da0e818d252e224c6b5bb4813252383a297
Benjamin Spain <benjamin@thespains.net>
2019-11-07 21:15:08 -0600

    Util and sample content

---
+++ util/nltFetch/main.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+const request = `http://api.nlt.to/api/passages?ref=${process.argv[2]}&key=${process.env.NLT_KEY}`;
+fetch(request)
+    .then(response => response.text())
+    .then(text => {
+        let bibletext = cheerio.load(text)('div[id="bibletext"]');
+        bibletext.removeAttr('id');
+
+        if (process.argv[3])
+        {
+            return encodeURIComponent(bibletext.toString());
+        }
+        else
+        {
+            return bibletext.toString();
+        }
+    });
+
