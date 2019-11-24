10175fd715be530605bfdba34f4d2f756721e817
Benjamin Spain <benjamin@thespains.net>
2019-11-24 10:50:13 -0600

    args settling

--- util/cli/main.ts
+++ util/cli/main.ts
@@ -11,28 +11,21 @@ program
     .description('Fetch a passage from NLT.TO.  Options allow to format and insert into a JSON series item.  Requires NLT_KEY environment variable to be set.')
     .requiredOption('-r, --reference <nlt reference>', 'NLT.TO passages reference (e.g. "John.1" or "Ezekiel.45:13-46:24" ')
     .action(function passageAction(options: PassageOptions) {
-        if (options.reference === '')
-        {
-            program.help();
-            process.exit();
-        }
-
         // Ensure NLT_API key is set
-        if (process.env.NLT_KEY === '')
+        if (process.env.NLT_KEY == undefined || process.env.NLT_KEY == '')
         {
-            console.error("Did not find nlt key at process.env.NLT_KEY.  Did you set it?")
+            console.error("Did not find nlt key at process.env.NLT_KEY.  Did you 'export NLT_KEY=<key>'")
             program.help();
             process.exit();
         }
 
-        console.info(program.opts());
         console.log("passageAction in progress...")
     });
 
 
 program.parse(process.argv);
 
-if (program.opts() == {})
+if (process.argv.length <= 2)
 {
     program.help();
 }
