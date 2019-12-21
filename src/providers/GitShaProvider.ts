237e55df93420d46af4de2d3171eaa9eee98c707
Benjamin Spain <2678462+bspain@users.noreply.github.com>
2019-12-21 12:17:41 -0800

    look for sha in prod (#22)

--- src/providers/GitShaProvider.ts
+++ src/providers/GitShaProvider.ts
@@ -1,21 +1,38 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { ConfigProvider } from './ConfigProvider';
 
-function GitSha() : string {
+class GitSha {
+    constructor(private config: ConfigProvider) { }
 
-    /*
-    * https://stackoverflow.com/questions/34518389/get-hash-of-most-recent-git-commit-in-node
-    */
+    fetchSha() : string {
+        if (this.config.env == 'production') {
+            return this.fetchProdSha()
+        }
 
-    const gitPath = path.join(__dirname, '../../../.git');
+        return this.fetchDevSha();
+    }
+    
+    private fetchProdSha() : string {
+        const activeDeployPath = path.join(__dirname, '../../../../deployments/active')
+        return fs.readFileSync(activeDeployPath).toString();
+    }
 
-    const gitHead = path.join(gitPath, 'HEAD')
-    const rev = fs.readFileSync(gitHead).toString();
-    if (rev.indexOf(':') === -1) {
-        return rev;
-    } else {
-        const gitRev = path.join(gitPath, rev.substring(5).trim())
-        return fs.readFileSync(gitRev).toString().trim();
+    private fetchDevSha() : string {
+        /*
+        * https://stackoverflow.com/questions/34518389/get-hash-of-most-recent-git-commit-in-node
+        */
+    
+       const gitPath = path.join(__dirname, '../../../.git');
+    
+       const gitHead = path.join(gitPath, 'HEAD')
+       const rev = fs.readFileSync(gitHead).toString();
+       if (rev.indexOf(':') === -1) {
+           return rev;
+       } else {
+           const gitRev = path.join(gitPath, rev.substring(5).trim())
+           return fs.readFileSync(gitRev).toString().trim();
+       }
     }
 }
 
