d9c45000200222bf305de609b755d6f5c0b4be32
Benjamin Spain <2678462+bspain@users.noreply.github.com>
2019-12-31 19:33:07 -0600

    Use passage in mobile button values (#31)
    * Mobile / Desktop split
    
    * Media query for desktop / mobile
    
    * Fixed nav banner in desktop
    
    * Click handling for banner nav
    
    * React unique key message
    
    * WIP
    
    * Padding to support nav banner
    
    * Remove title on passages
    
    * padding on desktop content
    
    * Use verses as values in mobile buttons
    
    * verify cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -67,7 +67,7 @@ class App extends React.Component<IAppProps, IAppState> {
         <ContentContainer entry={this.state.entry} selected={this.state.selected} />
         <MobileNavigationContainer
           expanded={this.state.navExpanded}
-          items={this.state.entry.navigation}
+          entry={this.state.entry}
           onClick={this.onContentNavClicked}
           onTextsize={this.onTextsizeClicked}
         ></MobileNavigationContainer>
--- client/src/components/navigation/mobile/__tests__/NavigationContainer.test.tsx
+++ client/src/components/navigation/mobile/__tests__/NavigationContainer.test.tsx
@@ -1,49 +1,88 @@
 /* global describe, it, expect */
+/* eslint @typescript-eslint/camelcase: 0 */
 import 'jest';
 import * as React from 'react';
 import { mount } from 'enzyme';
 import { NavigationContainer } from '../NavigationContainer';
 import { NavigationButton } from '../NavigationButton';
+import { EntryData } from '@crlife/Models';
 
 const handleClick = (): void => {};
 
 describe('NavigationContainer', () => {
   it('should render backward and forward nav buttons', () => {
+    const entry: EntryData = {
+      navigation: [],
+      content: [],
+      title: '',
+      subtitle: ''
+    };
+
     const wrapper = mount(
       <NavigationContainer
         onClick={handleClick}
         onTextsize={handleClick}
         expanded={true}
-        items={[]}
+        entry={entry}
       />
     );
+
     const buttons = wrapper.find(NavigationButton);
     expect(buttons).toHaveLength(2);
-
     expect(buttons.at(0).text()).toEqual('<');
     expect(buttons.at(1).text()).toEqual('>');
   });
 
-  it('should render each navigation item in order', () => {
-    const navItems = [
-      { ref: 'ref1', value: 'val1' },
-      { ref: 'ref2', value: 'val2' },
-      { ref: 'ref3', value: 'val3' }
-    ];
+  it('should render each navigation based on content', () => {
+    const entry: EntryData = {
+      navigation: [
+        { ref: 'ref1', value: 'val1' },
+        { ref: 'ref2', value: 'val2' },
+        { ref: 'ref3', value: 'val3' }
+      ],
+
+      content: [
+        {
+          id: 'ref1',
+          type: 'devotion',
+          title: 'title1',
+          api_nlt_to_ref: '',
+          value: 'There was a time'
+        },
+        {
+          id: 'ref2',
+          type: 'passage',
+          title: 'oldtest',
+          api_nlt_to_ref: '1Gen1',
+          value: 'There was a time'
+        },
+        {
+          id: 'ref3',
+          type: 'passage',
+          title: 'newtest',
+          api_nlt_to_ref: '1Matt2',
+          value: 'There was a time'
+        }
+      ],
+      title: '',
+      subtitle: ''
+    };
 
     const wrapper = mount(
       <NavigationContainer
         onClick={handleClick}
         onTextsize={handleClick}
         expanded={true}
-        items={navItems}
+        entry={entry}
       />
     );
+
     const buttons = wrapper.find(NavigationButton);
+
     expect(buttons).toHaveLength(5);
 
-    expect(buttons.at(0).text()).toEqual('val1');
-    expect(buttons.at(1).text()).toEqual('val2');
-    expect(buttons.at(2).text()).toEqual('val3');
+    expect(buttons.at(0).text()).toEqual('Devotional');
+    expect(buttons.at(1).text()).toEqual('1Gen1');
+    expect(buttons.at(2).text()).toEqual('1Matt2');
   });
 });
